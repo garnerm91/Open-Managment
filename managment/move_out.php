175f99828e85891baf9d0e357b641a63ac8f71ed
garnerm91 <garnerm91@gmail.com>
2017-02-20 18:20:53 -0500

    Add files via upload

--- managment/move_out.php
+++ managment/move_out.php
@@ -2,14 +2,25 @@
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
 $PageTitle="Open Managment";
-
+$fields = array('end_d');
+$error = false; //No errors yet
+foreach($fields AS $fieldname) { //Loop trough each field
+  if(!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) {
+    echo 'Field '.$fieldname.' misses!<br />'; //Display error with field
+    $error = true; //Yup there are errors
+  }
+}
+if(!$error) { //Only create queries when no error occurs
 $ten_id = $_GET['parent'];
 $end_d = $_POST["end_d"];
 
-$results = $mysqli->query("SELECT property_id FROM tenancy WHERE tent_id = '$ten_id' AND current = 1");
-while($row = $results->fetch_array()) {
- $property = $row["property_id"];
-}
+$stmt = $mysqli->prepare("SELECT property_id FROM tenancy WHERE tent_id = ? AND current = 1");
+$stmt->bind_param("s", $ten_id);
+$stmt->execute(); 
+$stmt->bind_result($property);
+$stmt->fetch();
+$stmt->close();
+
 $stmt1 = $mysqli->prepare("UPDATE tenant SET current='0' WHERE id=?;");
 $stmt1->bind_param("i", $ten_id);		
 $stmt1->execute();
@@ -24,9 +35,10 @@ $stmt3 = $mysqli->prepare("UPDATE tenancy SET current='0', end_date=? WHERE tent
 $stmt3->bind_param("si", $end_d, $ten_id);		
 $stmt3->execute();
 $stmt3->close();
- 		
+}
 function customPageHeader(){ }//add html to head here
 include_once('includes/header.php');
 
 
 include_once('includes/footer.php');
+
